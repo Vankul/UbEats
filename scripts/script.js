464d675ddc0efc782428bdd99fc8f584bd4ac4c2
Vankul <58622922+Vankul@users.noreply.github.com>
2022-06-01 13:32:16 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+top.onclick = function(event){
+    event.preventDefault();
+    document.body.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
+}
+
+
+const scrolBtn = document.querySelector('.is-show')
+window.onscroll = () => {
+    if (window.scrollY > 450){
+        scrolBtn.classList.remove('is-hide')
+    } else if (window.scrollY < 450){
+        scrolBtn.classList.add('is-hide');
+    }
+}
